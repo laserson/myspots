566a71d550e9dfbfbc418ab31be297b4ee6ec34e
Uri Laserson <uri.laserson@gmail.com>
2020-09-26 13:49:33 -0400

    Update airtable dependency

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email="uri.laserson@gmail.com",
     description="Search Google Maps for places and push to Airtable",
     url="https://github.com/laserson/myspots",
-    install_requires=["click", "pyyaml", "fastkml", "googlemaps", "airtable"],
+    install_requires=["click", "pyyaml", "fastkml", "googlemaps", "airtable-python-wrapper"],
     packages=["myspots"],
     entry_points={"console_scripts": ["myspots = myspots.cli:cli"]},
 )
